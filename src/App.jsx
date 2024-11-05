84fd6c1a9adbe650ebb93c516b81514c06c0d562
orangesoftware <robertogonzalezlozano@gmail.com>
2024-11-05 18:28:28 -0500

    changing var to const

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-var todoList = [
-                 {id:1,title:"Complete assignment"}
-                ,{id:2,title:"Complete assignment2"}
-                ,{id:3,title:"Complete assignment3"}];
+//import { useState } from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Todo List</h1>
-      <ul>
-        {
-          todoList.map((item)=>{
-            return <li id={item.id}>{item.title}</li>
-          })
-        }
-      </ul>
+     <h1>Todo List</h1>
+     <AddTodoForm />
+      <TodoList />
     </>
   )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,17 @@
-var todoList = [
-    {id:1,title:"Complete assignment"}
-   ,{id:2,title:"Complete assignment2"}
-   ,{id:3,title:"Complete assignment3"}];
+const todoList = [
+  { id: 1, title: "Complete assignment" }
+  , { id: 2, title: "Complete assignment2" }
+  , { id: 3, title: "Complete assignment3" }];
 
 function TodoList() {
   return (
-      <ul>
-        {
-          todoList.map((item)=>{
-            return <li id={item.id}>{item.title}</li>
-          })
-        }
-      </ul>
+    <ul>
+      {
+        todoList.map((item) => {
+          return <li id={item.id}>{item.title}</li>
+        })
+      }
+    </ul>
   )
 }
 
