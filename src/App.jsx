864d6a75705a6147d272dd006d6179056b531140
orangesoftware <robertogonzalezlozano@gmail.com>
2025-01-12 19:57:30 -0500

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from "./AddTodoForm";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState, useEffect, Fragment } from 'react';
 
 
@@ -66,9 +67,26 @@ function App() {
 
   return (
     <Fragment>
-     <h1>Todo List</h1>
-     <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? (<p>Loading...</p>) : (<TodoList todoList={todoList} onRemoveTodo={removeTodo}/>)}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <h1>Todo List</h1>
+            <AddTodoForm onAddTodo={addTodo} />
+            {isLoading ? (<p>Loading...</p>) : (<TodoList todoList={todoList} onRemoveTodo={removeTodo}/>)}
+          </>          
+        } />
+        <Route path="/new" element={
+          <>
+            <h1>New Todo List</h1>            
+          </>          
+        }
+        />
+      </Routes>
+    </BrowserRouter>
+
+
+     
     </Fragment>
   )
 }
